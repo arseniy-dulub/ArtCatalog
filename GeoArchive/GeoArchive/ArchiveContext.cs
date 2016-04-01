767bdde011886325bdf4e2d5f8d6f1cf3e06303a
arseni.dulub <dulub.ars@gmal.com>
2016-04-01 11:06:13 +0300

    + context

---
+++ GeoArchive/GeoArchive/ArchiveContext.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GeoArchive
+{
+    using System.Data.Entity;
+    using GeoArchive.Entity;
+
+    public class ArchiveContext : DbContext
+    {
+        public DbSet<GeoProject> GeoProjects { get; set; }
+    }
+}
