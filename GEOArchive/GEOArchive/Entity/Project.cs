daa548d1f8f4aa81abbfbae5ac50e702118216f8
arseni.dulub <dulub.ars@gmal.com>
2016-04-01 12:53:18 +0300

    added an entity Project and GeoFile

---
+++ GEOArchive/GEOArchive/Entity/Project.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEOArchive.Entity
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectNum { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectPerformer { get; set; }
+        public string ProjectCreateDate { get; set; }
+        public string ProjectDescription { get; set; }
+        public DateTime ProjectDateCreate { get; set; }
+
+        public virtual List<GeoFile> GeoFiles { get; set; }
+    }
+
+    public class GeoFile
+    {
+        public int GeoFileId { get; set; }
+        public string GeoFileType { get; set; }
+        public string GeoFilePath { get; set; }
+        public DateTime GeoFileDateCreate { get; set; }
+
+        public int ProjectId { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
