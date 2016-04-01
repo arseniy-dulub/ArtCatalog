5e92ee360a3aa363c22ce581e9691edbc47633a0
arseni.dulub <dulub.ars@gmal.com>
2016-04-01 11:13:31 +0300

    ++

---
+++ GeoArchive/GeoArchive/Tools/GeoProjectManager.cs
@@ -0,0 +1,44 @@
+﻿
+namespace GeoArchive.Tools
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Data.Entity;
+    using System.Data;
+    using GeoArchive.Entity;
+    using System.IO;
+    using System.Windows.Forms;
+
+    public class GeoProjectManager
+    {
+        private readonly IArchiveRepository _archiveRepository;
+
+        private ArchiveContext db = new ArchiveContext();
+
+        public GeoProjectManager() : this(new ArchiveRepository()) { }
+
+        public GeoProjectManager(IArchiveRepository archiveRepository)
+        {
+            this._archiveRepository = archiveRepository;
+        }
+
+        public bool Create(string uploadFiles, string firstFile)
+        {
+            if (uploadFiles != string.Empty)
+            {
+                GeoProject newGeoProject = new GeoProject();
+                newGeoProject.Name = FileManager.GetFileName(firstFile);
+                newGeoProject.Files = uploadFiles;
+
+                db.GeoProjects.Add(newGeoProject);
+                db.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
